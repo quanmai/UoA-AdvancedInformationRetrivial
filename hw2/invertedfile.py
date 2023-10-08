95fc3081771d87856fe06ea0ef310f90e2171262
Quan Mai <40239882+quanmai@users.noreply.github.com>
2023-10-08 18:14:42 -0500

    Using hashmap for GlobalDict

--- hw2/invertedfile.py
+++ hw2/invertedfile.py
@@ -12,7 +12,70 @@ import collections
 from ply.lex import TOKEN
 import time, timeit
 import subprocess
+from typing import NamedTuple, Tuple, List
+import atexit
 
+POSTFILE = "post_file.txt"
+DICTFILE = "dict_file.txt"
+MAPPFILE = "mapping_file.txt"
+EMPTYSTR = "_empty"
+KEYS = 500
+
+class StringIntPair(NamedTuple):
+    key: str
+    data: List[Tuple[int, int]] # doc_id, freq
+
+class HashTable:
+    def __init__(self, num_keys: int, num_files: int):
+        self.empty_str = EMPTYSTR
+        self.size = num_keys * num_files * 3 # save 2/3 for linear probing
+        self.used = 0
+        self.collisions = 0
+        self.lookups = 0
+        self.hashtable = [StringIntPair(self.empty_str, (-1, -1))] * self.size
+        atexit.register(self.cleanup)
+
+    def cleanup(self):
+        self.hashtable.clear()
+
+    def insert(self, key: str, data: tuple):
+        index = -1
+        if self.used >= self.size:
+            print("The hashtable is full; cannot insert.")
+        else:
+            index = self.hash(key)
+            if self.hashtable[index].key == self.empty_str:
+                self.hashtable[index] = StringIntPair(key, [data])
+            else:
+                self.hashtable[index].data.append(data)
+            self.used += 1
+
+    def getData(self, key: str) -> List[Tuple]:
+        self.lookups += 1
+        index = self.hash(key)
+        if self.hashtable[index].key == self.empty_str:
+            return -1
+        else:
+            return self.hashtable[index].data
+
+    def __iter__(self):
+        return ((k, v) for (k, v) in self.hashtable)
+    
+    def hash(self, key):
+        sum = 0
+        # add all the characters of the key together
+        for k in key:
+            sum = (sum * 19) + ord(k)   # Mult sum by 19, add byte value of char
+   
+        index = sum % self.size
+
+        # linear probing
+        while self.hashtable[index].key != self.empty_str and self.hashtable[index].key != key:
+            index = (index + 1) % self.size
+            self.collisions += 1
+       
+        return index
+    
 # list of TOKENS (required)
 tokens =[
     'EMAIL',
@@ -130,12 +193,12 @@ def MyLexer():
     return lex.lex()
 
 # global_dict: list((doc_id, rfreq))
-def read_one_file(global_dict: dict, lexer: callable, 
+def read_one_file(global_dict: HashTable, lexer: callable, 
                   file_name: str, doc_id: int, raw_freq: bool = True):
-    dict = collections.defaultdict(list)
+    dict = collections.defaultdict(int)
     num_tokens = 0
     with open(file_name, 'r') as rf:
-        # print("Process file: ", file_name)
+        print("Process file: ", file_name)
         lines = rf.readlines()
         for line in lines:
             try:
@@ -149,8 +212,7 @@ def read_one_file(global_dict: dict, lexer: callable,
                 break
     for k, v in dict.items():
         freq = v if raw_freq else v/num_tokens
-        global_dict[k].append((doc_id, freq))
-    del(dict) 
+        global_dict.insert(k, (doc_id, freq))
     return num_tokens
 
 if __name__ == '__main__':
@@ -166,10 +228,6 @@ if __name__ == '__main__':
             the document id
             the filename
     """
-    POSTFILE = "post_file.txt"
-    DICTFILE = "dict_file.txt"
-    MAPPFILE = "mapping_file.txt"
-
     lexer = MyLexer()
     inputDir = sys.argv[1]
     outputDir = sys.argv[2]
@@ -189,9 +247,9 @@ if __name__ == '__main__':
 
     # time_start = timeit.default_timer()
     time_start = time.process_time()
-    globaldict = collections.defaultdict(list)
     corpora_size = 0
     number_of_file = 1750
+    globaldict = HashTable(KEYS, number_of_file) #collections.defaultdict(list)
     f = open(os.path.join(outputDir, MAPPFILE), 'w') # write to mapping file
     f.write("DOCID \t FREQUENCY")
     for i, file_name in enumerate(list_of_files[:number_of_file]):
@@ -202,20 +260,24 @@ if __name__ == '__main__':
     # write to dict file
     f = open(os.path.join(outputDir, DICTFILE), 'w') # write to mapping file
     f.write("TERM \t NUMDOC \t START")
-    output_dict = collections.defaultdict(tuple)
     start = 0
-    for k, v in globaldict.items():
-        num_docs = len(globaldict[k])
-        start += num_docs
-        output_dict[k] = (num_docs, start)
-        f.write("\n{}\t{}\t{}".format(k, num_docs, start))
+    for k, v in globaldict:
+        if k == globaldict.empty_str:
+            n, s = v
+            f.write("\n{}\t{}\t{}".format(k, n, s))
+        else:
+            num_docs = len(v)
+            start += num_docs
+            f.write("\n{}\t{}\t{}".format(k, num_docs, start))
 
     # write to post file
     f = open(os.path.join(outputDir, POSTFILE), 'w')
     f.write("DOCID \t FREQUENCY")
-    for v in globaldict.values():
-        for doc_id, freq in v:
-            f.write("\n{} \t {}".format(doc_id, freq))
+    for k, v in globaldict:
+        if k != globaldict.empty_str:
+            for doc_id, freq in v:
+                f.write("\n{} \t {}".format(doc_id, freq))
     f.close()
+    print(globaldict.hash("commensurate"))
     print('Time run for {} files: {}'.format(number_of_file, time.process_time() - time_start))
 
